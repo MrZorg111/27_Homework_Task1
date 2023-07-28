7f6b72e5def6665a21f655aa67e42daeb6f6c08a
MrZorg111 <yodji333@mail.ru>
2023-07-28 19:57:35 +0300

    Подготовка средней ветви завершена

--- class/MediumBranch.h
+++ class/MediumBranch.h
@@ -1,1 +1,13 @@
 #pragma once
+#include <string>
+#include <vector>
+#include "Branch.h"
+
+class MediumBranch : public Branch {
+	const int SIZE_HOUSE = 5;
+public:
+	//Геттерс для достума к количеству домов на ветке
+	int getSizeHouse() {
+		return SIZE_HOUSE;
+	}
+};
